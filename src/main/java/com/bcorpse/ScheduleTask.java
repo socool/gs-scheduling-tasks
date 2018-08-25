0f1325df46948932a6b5346ebf1df81bda8728b6
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-08-25 21:27:21 +0700

    add various schedule case

--- src/main/java/com/bcorpse/ScheduleTask.java
+++ src/main/java/com/bcorpse/ScheduleTask.java
@@ -19,4 +19,26 @@ public class ScheduleTask {
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
+
+    @Scheduled(fixedDelay = 1000)
+    public void scheduleFixedDelayTask() {
+        System.out.println(
+                "Fixed delay task - " + System.currentTimeMillis() / 1000);
+    }
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 1000)
+    public void scheduleFixedRateWithInitialDelayTask() {
+
+        long now = System.currentTimeMillis() / 1000;
+        System.out.println(
+                "Fixed rate task with one second initial delay - " + now);
+    }
+
+    @Scheduled(cron = "0 15 10 15 * ?")
+    public void scheduleTaskUsingCronExpression() {
+
+        long now = System.currentTimeMillis() / 1000;
+        System.out.println(
+                "schedule tasks using cron jobs - " + now);
+    }
 }
